74a379615a335a7c84363c6394d64b52675da587
Kevin Cox <kevincox@kevincox.ca>
2014-04-12 17:41:53 -0400

    Precompress files.

--- build.sh
+++ build.sh
@@ -6,10 +6,33 @@ rm -rvf "$tdir"
 
 echo 'Merging scripts...'
 r.js -o 'build.js' "dir=$tdir/a/$ver/"
+rm "$tdir/a/$ver/build.txt" # Remove r.js's file.
 
 echo 'Generating index.html...'
 sed -e "s_/a/js/_/a/$ver/js/_g" 'index.html' | tr -d $'\n' > "$tdir/index.html"
 
+echo 'Gzipping content...'
+ignore=(
+	'-not' '-name' '*.map'
+	'-not' '-name' '*.src.js'
+)
+find "$tdir" -type f "${ignore[@]}" -print0 | parallel -0v 'gzip -nk --best {}'
+echo 'Pruning uncompressable content...'
+shopt -s globstar
+for c in $tdir**/*.gz; do
+	o="${c%%.gz}"
+	[ -f "$o" ] || continue # Skip if we don't have the source.
+	
+	cs=$(stat -c %s "$c")
+	os=$(stat -c %s "$o")
+	if [ $(bc -l <<<"$cs/$os > 0.95") -ne 0 ]; then
+		echo "Removing '$c' with ratio $(bc -l <<<"$cs/$os")"
+		rm "$c"
+	fi
+done
+
+echo 'Copying server-side files...'
 cp -vr '.htaccess' 'noscript/' "$tdir/"
 echo 'Adding build id to router.php...'
-perl -pe "s/(^\s*\\\$buildid\s*=).*$/\$1 \"$ver\";/" 'noscript/router.php' > "$tdir/noscript/router.php"
+perl -pe "s/(^\s*\\\$buildid\s*=).*$/\$1 \"$ver\";/" \
+         'noscript/router.php' > "$tdir/noscript/router.php"
