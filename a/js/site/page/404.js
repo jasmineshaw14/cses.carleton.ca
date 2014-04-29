a457e9228e4c03a728c6f02e126efb5d2008cd8a
Kevin Cox <kevincox@kevincox.ca>
2014-04-28 22:45:25 -0400

    Fix 404 page.

--- a/js/site/page/404.js
+++ a/js/site/page/404.js
@@ -4,15 +4,28 @@ function($, mkstatic)
 	"use strict";
 	
 	var content = $("<div>", {
-		html:'<p style="margin:1em;font-size:1.7em">404: I looked really hard but nothing came up.</p>'+
-		     '<a href="http://comicjk.com/comic.php/404" class="block">'+
-		     '<img src="http://comicjk.com/Pics/Comic404.gif" style="max-width:100%"/></a>',
-		class: "content",
 		css: {
 			padding: "15px",
 			textAlign: "center",
 		},
-	});
+	}).append(
+		$("<p>", {
+			text: "404: I looked really hard but nothing came up.",
+			css: {
+				margin: "1em",
+				fontSize: "1.7em",
+			},
+		}),
+		$("<a>", {
+			href: "http://comicjk.com/comic.php/404",
+			css: {display: "block"},
+		}).append(
+			$("<img>", {
+				src: "http://comicjk.com/Pics/Comic404.gif",
+				css: {maxWidth: "100%"},
+			})
+		)
+	);
 	
 	return mkstatic("404 Not Found — CSES", content);
 });
