8b0fa92137eca756679721e5df8112a59f39bd1b
Kevin Cox <kevincox@kevincox.ca>
2014-05-08 19:47:42 -0400

    Set title on pages from database.

--- a/js/site/templates.js
+++ a/js/site/templates.js
@@ -2,6 +2,7 @@ define(["jquery"], function($){
 	var self = Object.create(Object.prototype, {
 		article: {
 			value: function template_article($cont, page){
+				document.title = page.title + " — CSES";
 				$cont.append(page.content.children());
 			},
 		},
