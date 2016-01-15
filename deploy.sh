8a865ddefcd852e3a3bcc87cc3734f48867c9c78
Kevin Cox <kevincox@kevincox.ca>
2016-01-15 17:02:06 -0500

    Make deploys more deterministic.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 tdir="${1:-generated}"
 
-rsync -rvz --chmod 775 "$tdir/" kevincox@newstout.engsoc.org:/srv/http/cses.carleton.ca/
+rsync -rvzp --chmod 775 "$tdir/" kevincox@newstout.engsoc.org:/srv/http/cses.carleton.ca/
