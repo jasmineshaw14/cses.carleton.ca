3b1ffd415c102e0cc4507188e37e78d3ac53e9d6
Kevin Cox <kevincox@kevincox.ca>
2016-01-21 11:27:28 -0500

    Allow site to work with older apache.

--- bin/deploy-openshift.sh
+++ bin/deploy-openshift.sh
@@ -3,16 +3,17 @@
 set -e
 
 tdir="${1:-generated}"
+repo='ssh://5699768d89f5cf1123000118@web-cses.rhcloud.com/~/git/web.git/'
 
 if [ -n "$CI" ]; then
 	git config --global user.email 'robot@cses.carleton.ca'
 	git config --global user.name 'Robot'
 fi
 
-mkdir deploy
+
+git clone "$repo" deploy
 cp -r "$tdir" deploy/public_html
 cd deploy
-git init
-git add .
+git add -A
 git commit -m 'Deploy'
-git push --force ssh://5699768d89f5cf1123000118@web-cses.rhcloud.com/~/git/web.git/ master
+git push
