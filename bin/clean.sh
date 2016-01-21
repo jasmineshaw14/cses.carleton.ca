e52ec51fcd4f2b1fffefa4552692c874f27f7b65
Kevin Cox <kevincox@kevincox.ca>
2016-01-21 14:23:40 -0500

    Add clean script.

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,13 +1,30 @@
 #! /bin/bash
 
-set -ex
+set -e
 shopt -s nullglob
 
 tdir="${1:-generated}/a"
-age="${CLEAN_AGE:-3 days ago}"
+age="${CLEAN_AGE:-0}"
 
 builds=("$tdir"/*)
 
 [ "${#builds[@]}" -le 2 ] && exit
 
 from="${builds[-2]}"
+fromdate="$(cat "$from/.build-date")"
+cutoff=$(($fromdate - $age*24*60*60))
+
+echo "Removing all builds before $(date -u "-d@$cutoff")"
+
+for b in "${builds[@]}"; do
+	if [ '!' -e "$b/.build-date" ]; then
+		echo "Removing $b due to missing build metadata."
+		rm -r "$b"
+	elif [ "$(cat "$b/.build-date")" -lt "$cutoff" ]; then
+		echo "Removing $b due to age."
+		rm -r "$b"
+	fi
+done
+
+echo "Remaining builds:"
+du -hd1 "$tdir"
