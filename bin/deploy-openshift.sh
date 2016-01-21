07b22ee3d19d944bb6d4f5ea942b111de5dcf629
Kevin Cox <kevincox@kevincox.ca>
2016-01-21 14:26:09 -0500

    Clean openshift deploys.

--- bin/deploy-openshift.sh
+++ bin/deploy-openshift.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -e
+set -ex
 
 tdir="${1:-generated}"
 repo='ssh://5699768d89f5cf1123000118@web-cses.rhcloud.com/~/git/web.git/'
@@ -10,9 +10,11 @@ if [ -n "$CI" ]; then
 	git config --global user.name 'Robot'
 fi
 
-
 git clone "$repo" deploy
 cp -r "$tdir" deploy/public_html
+
+bin/clean.sh "$deploy"
+
 cd deploy
 git add -A
 git commit -m 'Deploy'
